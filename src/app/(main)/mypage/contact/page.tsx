9f5c532a31480633f12df6df60ad03591889891e
dahyuniiiiii <imcute0703123@naver.com>
2025-10-10 15:29:46 +0900

    Feat: 마이페이지 구현 및 하위페이지 라우팅 구조 정리

---
+++ src/app/(main)/mypage/contact/page.tsx
@@ -0,0 +1,5 @@
+export const metadata = { title: "문의하기페이지" };
+
+export default function contactPage() {
+  return <div>문의하기페이지 준비중</div>; // 임시 UI
+}
---
+++ src/app/(main)/mypage/editprofile/page.tsx
@@ -0,0 +1,5 @@
+export const metadata = { title: "프로필변경페이지" };
+
+export default function editprofilePage() {
+  return <div>프로필변경페이지 준비중</div>; // 임시 UI
+}
