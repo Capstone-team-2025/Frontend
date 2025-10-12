ea1615fdb2cd82a485f747bbd0294ce60639bf51
dahyuniiiiii <imcute0703123@naver.com>
2025-10-12 23:45:16 +0900

    Fix: 마이페이지 프로필 ui 개선 및 이동 기능 추가

--- src/app/(main)/mypage/setmembership/page.tsx
+++ src/app/(main)/mypage/setmembership/page.tsx
@@ -4,7 +4,7 @@ export default function setMembershipPage() {
   return (
     <div>
       <Header title="통신사/등급 재설정" />
-      ;통신사/등급 재설정 페이지 준비중
+      통신사/등급 재설정 페이지 준비중
     </div>
   ); // 임시 UI
 }
