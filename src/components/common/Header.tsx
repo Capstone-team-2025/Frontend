b329604640e9934ef266cb8d8ec7b34e823bfc11
dahyuniiiiii <imcute0703123@naver.com>
2025-10-15 13:59:50 +0900

    Feat: 마이페이지 뒤로가기 기능

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,43 @@
 "use client";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function Header({ title }: { title: string }) {
   const pathname = usePathname();
+  const router = useRouter();
 
-  let bgColor = "bg-white"; 
-  if (pathname.startsWith("/mypage/editprofile")|| pathname.startsWith("/mypage/favorites")) {
-    bgColor = "bg-[#FB4E6F]"; 
-  } 
+  let bgColor = "bg-white";
+  if (
+    pathname.startsWith("/mypage/editprofile") ||
+    pathname.startsWith("/mypage/favorites")
+  ) {
+    bgColor = "bg-[#FB4E6F]";
+  }
+  const showBackButton =
+    pathname.startsWith("/mypage/") && pathname !== "/mypage";
+  const isPink = bgColor === "bg-[#FB4E6F]";
+  const backIcon = isPink ? "/images/WBack.svg" : "/images/BBack.svg";
+  const textColor = isPink ? "text-white" : "text-gray-700";
   return (
-    <header className={`sticky px-3 py-5 ${bgColor}`}>
-      <h1 className="flex justify-center font-bold text-xl ">{title}</h1>
+    <header
+      className={`sticky top-0 z-50 flex items-center justify-center px-3 py-5 ${bgColor}`}
+    >
+      {showBackButton && (
+        <button
+          onClick={() => router.back()}
+          className="absolute left-4 flex items-center justify-center"
+          aria-label="뒤로가기"
+        >
+          <Image
+            src={backIcon}
+            alt="뒤로가기 버튼"
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+        </button>
+      )}
+      <h1 className={`font-bold text-xl text-center ${textColor}`}>{title}</h1>
     </header>
   );
 }
